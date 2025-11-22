ed210875928de43aba0a042c250ac9b46ccf7911
nitesh011y <niteshshedge011@gmail.com>
2025-11-22 22:09:21 +0530

    done

--- frontend/src/Chatwindow.jsx
+++ frontend/src/Chatwindow.jsx
@@ -24,14 +24,17 @@ function Chatwindow() {
     setNewChat(false);
 
     try {
-      const response = await fetch("http://localhost:4000/GPT/chat", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          message: prompt,
-          threadId: currThreadId,
-        }),
-      });
+      const response = await fetch(
+        "https://gpt-backend-xyz.onrender.com/GPT/chat",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            message: prompt,
+            threadId: currThreadId,
+          }),
+        }
+      );
 
       const res = await response.json();
       console.log(res);
