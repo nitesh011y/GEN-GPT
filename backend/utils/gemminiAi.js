fedd955fffc3c7c666a034d5e132c0979d599485
nitesh011y <niteshshedge011@gmail.com>
2026-02-11 17:46:39 +0530

    Update API endpoint for Gemini model

--- backend/utils/gemminiAi.js
+++ backend/utils/gemminiAi.js
@@ -19,7 +19,7 @@ const getGemAIAPIResponse = async function (message) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       options
     );
 
